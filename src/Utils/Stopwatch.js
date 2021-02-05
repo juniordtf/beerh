b5eba2db3bee37502e8a7a7fd92e0e102b3ca4ec
Djalma <juniordtf@gmail.com>
2021-02-04 23:36:01 -0300

    Fixed stopwatch component

--- src/Utils/Stopwatch.js
+++ src/Utils/Stopwatch.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, Platform} from 'react-native';
+import {Text, StyleSheet, Platform, AppState} from 'react-native';
 import BackgroundTimer from 'react-native-background-timer';
+import AsyncStorage from '@react-native-community/async-storage';
+import {differenceInSeconds} from 'date-fns';
 
 class Stopwatch extends Component {
   constructor(props) {
@@ -21,7 +23,18 @@ class Stopwatch extends Component {
       pausarHabilitado: false,
       pararHabilitado: false,
       zerarHabilitado: false,
+      elapsed: 0,
+      appState: AppState.currentState,
+      isActive: true,
     };
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
   }
 
   addSecond = () => {
@@ -79,6 +92,9 @@ class Stopwatch extends Component {
     }
 
     let intervalId = BackgroundTimer.setInterval(this.addSecond, 1000);
+
+    this.recordStartTime();
+
     this.setState({
       intervalId: intervalId,
 
@@ -100,11 +116,13 @@ class Stopwatch extends Component {
 
   stopStopwatch = () => {
     BackgroundTimer.clearInterval(this.state.intervalId);
+    this.clearStartTime();
 
     this.setState({
       pausarHabilitado: false,
       pararHabilitado: false,
       zerarHabilitado: true,
+      isActive: false,
     });
   };
 
@@ -126,6 +144,76 @@ class Stopwatch extends Component {
       contadorHora: parseInt(time.slice(0, 2), 10),
     });
   }
+
+  recordStartTime = async () => {
+    try {
+      const now = new Date();
+      await AsyncStorage.setItem('@start_time', now.toISOString());
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
+
+  recordStartTime = async () => {
+    try {
+      const now = new Date();
+      console.log(now);
+
+      await AsyncStorage.setItem('@start_time', now.toISOString());
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
+
+  clearStartTime = async () => {
+    try {
+      await AsyncStorage.setItem('@start_time', 0);
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
+
+  handleAppStateChange = async (nextAppState) => {
+    let elapsed = this.state.elapsed;
+
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      // We just became active again: recalculate elapsed time based
+      // on what we stored in AsyncStorage when we started.
+      elapsed = await this.getElapsedTime();
+      // Update the elapsed seconds state
+    }
+
+    this.setState({
+      elapsed: elapsed,
+      appState: nextAppState,
+    });
+
+    if (elapsed > 0) {
+      let display = new Date(elapsed * 1000).toISOString().substr(11, 8);
+      this.setState({
+        contadorSegundo: parseInt(display.slice(6, 8), 10) + 1,
+        contadorMinuto: parseInt(display.slice(3, 5), 10),
+        contadorHora: parseInt(display.slice(0, 2), 10),
+      });
+    }
+  };
+
+  getElapsedTime = async () => {
+    try {
+      const startTime = await AsyncStorage.getItem('@start_time');
+      const now = new Date();
+      return differenceInSeconds(now, Date.parse(startTime));
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
 
   render() {
     return <Text style={styles.bodyText}>{this.showDisplay()}</Text>;
