16cc38b7a666ea93385441d17708448888957934
juniordtf <juniordtf@gmail.com>
2024-09-13 17:39:17 -0300

    changed ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://192.168.2.124:8001/v1'});
+const api = axios.create({baseURL: 'http://192.168.0.202:8001/v1'});
 
 export default api;
